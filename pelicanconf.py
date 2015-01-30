5f9c75079cf37830c93e3551ad4157a8de78b313
Felix Ingram <f.ingram@gmail.com>
2015-01-30 16:37:25 +0000

    Publishing on 2015-01-30 16:37:25.709000

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,8 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
 
+THEME = r'../pelican-themes/monospace'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
